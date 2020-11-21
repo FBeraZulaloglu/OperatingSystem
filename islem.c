6115c1b448e8e16c88a51573bee7d8ad4116bc19
Faruk Bera <farukzulal5886@gmail.com>
2020-11-21 12:39:32 -0500

    added malloc for arguments in islem

--- islem.c
+++ islem.c
@@ -3,16 +3,24 @@
 #include<stdlib.h> 
 #include<unistd.h> 
 #include<sys/wait.h> 
-#include<sys/types.h> 
+#include<sys/types.h>
+
+#define MAXPARAM 100
 
 int main(int argc, char *argv[]){
     //printf("ISLEM : %s\n",argv[0]);
     //printf("NUMBERS : %s,%s\n",argv[1],argv[2]);
-    char *newargv[3];
+    
     int stat,result;
-    newargv[0] = argv[1];
-    newargv[1] = argv[2];
-    newargv[2] = NULL;
+    char **newargv = (char **)malloc(MAXPARAM*sizeof(char *));
+
+    for (int i = 0; i < MAXPARAM; i++)
+    {
+        newargv[i] = (char *)malloc(MAXPARAM* sizeof(char));
+    }
+    strcpy(newargv[0],argv[1]); // cikar or topla
+    strcpy(newargv[1],argv[2]);
+    
     pid_t p = fork();
     if(p == 0){
         if(!strcmp(argv[0],"cikar")){
